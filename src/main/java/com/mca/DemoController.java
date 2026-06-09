a00cf033dbed8113911ab94ef5be0449b56a8d4f
Ritesh <riteshsaud@gmail.com>
2026-06-09 11:26:33 +0530

    new code

--- src/main/java/com/mca/DemoController.java
+++ src/main/java/com/mca/DemoController.java
@@ -1,35 +1,45 @@
 package com.mca;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 	
 	@GetMapping("/fetch")
-	
-	public String fetch() {
-		return "Data fethed";
+	public String fetch(@RequestParam(name = "pname") String name,
+			@RequestParam(required = false, defaultValue = "20") Integer age) {
+		System.out.println(name);
+		System.out.println(age);
+		return name+" ,"+age;
 	}
 	
 	@PostMapping("/save")
-	public String save() {
-		
-		return "data saved";
+	public UserData save(@RequestBody UserData user) {
+		System.out.println(user);
+		return user;
 	}
 	
-	@PutMapping("/update")
-	public String update() {
-		
-		return "data saved";
+	@PutMapping("/update/{name}/{age}")
+	public String update(@PathVariable String name, @PathVariable int age) {
+		System.out.println(name);
+		System.out.println(age);
+		return name + " , " + age;
 	}
 	
 	@DeleteMapping("/delete")
-	public String delete() {
-		return "delete data";
+	public String delete(@RequestHeader String name,@RequestHeader int age) {
+		System.out.println(name);
+		System.out.println(age);
+		return name+" , "+age;
 	}
 }
